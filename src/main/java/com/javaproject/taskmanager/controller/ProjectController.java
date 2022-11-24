e72a23d37a3464835393d6fce2ac0ecbdf244c33
Shiori Fujii <71534267+shiori793@users.noreply.github.com>
2022-11-23 20:15:00 -0800

    Modify degression (#9)
    * modify degression
    
    * move /project get mapping to project controller, add code to get user information from the session

--- src/main/java/com/javaproject/taskmanager/controller/ProjectController.java
+++ src/main/java/com/javaproject/taskmanager/controller/ProjectController.java
@@ -1,5 +1,9 @@
 package com.javaproject.taskmanager.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,24 +18,38 @@ import com.javaproject.taskmanager.repository.ProjectRepository;
 @Controller
 @RequestMapping("/project")
 public class ProjectController {
+
+   @Autowired
+   HttpSession session;
+
    @Autowired
    ProjectRepository projectRepository;
 
+   @GetMapping("/project")
+   public String showProject(Model model) {
+         model.addAttribute("user", session.getAttribute("user"));
+         List<Project> projects = projectRepository.findAll();
+         model.addAttribute("projects", projects);
+         return "project/project";
+   }
+
    @GetMapping("/new")
    public String displayProjectFrom(Model model) {
-    model.addAttribute("project", new Project());
-    return "project/new-project";
+      model.addAttribute("user", session.getAttribute("user"));
+      model.addAttribute("project", new Project());
+      return "project/new-project";
    }
 
    @PostMapping("/save")
    public String saveProject(Project project, Model model) {
-    projectRepository.save(project);
-    System.out.println(project.getId());
-    return "redirect:/project";
+      projectRepository.save(project);
+      System.out.println(project.getId());
+      return "redirect:/project";
    }
 
    @GetMapping("/edit/{id}")
-    public String editProject(@PathVariable Long id, Model model) {
+   public String editProject(@PathVariable Long id, Model model) {
+      model.addAttribute("user", session.getAttribute("user"));
       Project project = projectRepository.findById(id).get();
       model.addAttribute("project", project);
       return "project/edit-project";

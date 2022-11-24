b2e2fcae195482b0c7c00040cea25481a3dcde4a
Shiori Fujii <71534267+shiori793@users.noreply.github.com>
2022-11-24 08:23:37 -0800

    modify ptroject part entity, controller, html (#10)

--- src/main/java/com/javaproject/taskmanager/domain/Project.java
+++ src/main/java/com/javaproject/taskmanager/domain/Project.java
@@ -4,30 +4,43 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
-@Data
+@Getter
+@Setter
 public class Project {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "project_id")
     private long id;
 
+    @Column(name = "project_name")
     private String name;
+
+    @Column(name = "project_description")
     private String description;
-    private String status;
+
+    @Column(name = "project_status")
+    private int status;
 
     @OneToMany(cascade = CascadeType.ALL)
     private List<Task> taskList  = new ArrayList<>();
 
-    public Project(long id, String name, String description, String status) {
+    @ManyToMany(mappedBy = "projectList")
+    private List<ProjectUser> userList  = new ArrayList<>();
+
+    public Project(long id, String name, String description, int status) {
         this.id = id;
         this.name = name;
         this.description = description;
@@ -38,27 +51,27 @@ public class Project {
    
     }
 
-    public String getName() {
-        return name;
-    }
+    // public String getName() {
+    //     return name;
+    // }
 
-    public String getDescription() {
-        return description;
-    }
+    // public String getDescription() {
+    //     return description;
+    // }
 
-    public String getStatus() {
-        return status;
-    }
+    // public String getStatus() {
+    //     return status;
+    // }
 
-    public void setStatus(String status) {
-        this.status = status;
-    }
+    // public void setStatus(String status) {
+    //     this.status = status;
+    // }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+    // public void setDescription(String description) {
+    //     this.description = description;
+    // }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    // public void setName(String name) {
+    //     this.name = name;
+    // }
 }

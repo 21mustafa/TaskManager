0b76170fe21df51773a79da9aeb52b4136ae21f2
Shiori Fujii <71534267+shiori793@users.noreply.github.com>
2022-11-24 16:55:17 -0800

    modify the query in project repository (#16)

--- src/main/java/com/javaproject/taskmanager/repository/ProjectRepository.java
+++ src/main/java/com/javaproject/taskmanager/repository/ProjectRepository.java
@@ -13,7 +13,7 @@ public interface ProjectRepository extends CrudRepository<Project, Long>{
     @Override
     public List<Project> findAll();
 
-    @Query(nativeQuery=true, value="SELECT j.project_id, p.project_name, p.project_description, p.project_status FROM user_project j LEFT JOIN project p ON j.project_id = p.project_id WHERE user_id = :userId;")
+    @Query(nativeQuery=true, value="SELECT p.* FROM project p RIGHT JOIN user_project j ON p.project_id = j.project_id WHERE j.user_id = :userId")
     public List<Project> findAllProjectsByUserId(@Param("userId") long userId);
 
 }

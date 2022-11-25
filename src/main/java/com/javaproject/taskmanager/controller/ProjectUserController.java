c51dabbaacc623e98c692b809bc22848dbc6582c
Shiori Fujii <71534267+shiori793@users.noreply.github.com>
2022-11-25 08:45:13 -0800

    Modify get session (#21)
    * modify the way to user information from the session in every controller
    
    * modify login function

--- src/main/java/com/javaproject/taskmanager/controller/ProjectUserController.java
+++ src/main/java/com/javaproject/taskmanager/controller/ProjectUserController.java
@@ -1,5 +1,8 @@
 package com.javaproject.taskmanager.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +12,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.javaproject.taskmanager.domain.ProjectUser;
+import com.javaproject.taskmanager.domain.Task;
 import com.javaproject.taskmanager.repository.ProjectUserRepository;
+import com.javaproject.taskmanager.repository.TaskRepository;
 
 @Controller
 public class ProjectUserController {
@@ -24,6 +29,9 @@ public class ProjectUserController {
     @Autowired
     ProjectUserRepository projectUserRepository;
 
+    @Autowired
+    TaskRepository taskRepository;
+
     @GetMapping("/login")
     public String showLogin(Model model) {
         model.addAttribute("user", new ProjectUser());
@@ -31,12 +39,17 @@ public class ProjectUserController {
     }
 
     @PostMapping("/login")
-    public String login(Model model, ProjectUser user) {
+    public String login(Model model, ProjectUser user, HttpServletRequest request) {
         ProjectUser loginUser = projectUserRepository.findByEmailAndPassword(user.getEmail(), user.getPassword());
         if (loginUser != null) {
+            session.invalidate();
+            session = request.getSession();
             session.setAttribute("user", loginUser);
-            // model.addAttribute("user", loginUser);
-            return "redirect:/";
+            model.addAttribute("user", loginUser);
+            List<Task> taskList = taskRepository.findByUserAndTaskStatusOrderByTaskDeadline(loginUser, 0);
+            model.addAttribute("mytasks", taskList);
+            return "home/home";
+            // return "redirect:/";
         }
         return "redirect:/login";
     }
@@ -55,7 +68,8 @@ public class ProjectUserController {
 
     @GetMapping("/signout")
     public String signout() {
-        session.removeAttribute("user");
+        session.invalidate();
+        // session.removeAttribute("user");
         return "redirect:/login";
     }
 
